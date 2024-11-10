9af54c3ba460f9ff70d1103c4720b0a3602cfe44
Omesh Balajirao Kete <116993296+OmeshKete@users.noreply.github.com>
2024-11-10 11:06:57 +0530

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -2,6 +2,6 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/OmeshKete/ok-netflix-clone.git
-mv ok-netflix-clone/* .
+git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
+mv pwj-netflix-clone/* .
 tail -100f /var/log/httpd/access_log
